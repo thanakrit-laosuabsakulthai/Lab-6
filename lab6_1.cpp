d304332a9fa27343129df448f734aab2468b050c
Thanakrit <thanakrit_la@cmu.ac.th>
2025-12-09 15:18:51 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+int main()
+{
+	int input_integer;
+	int even_number_count = 0;
+	int odd_number_count = 0;
+	
+	cout << "Enter an integer: ";
+	cin >> input_integer;
+	
+	while (input_integer != 0)
+	{
+		// Process the input_integer to count even and odd numbers
+		if (input_integer % 2 == 0)
+		{
+			even_number_count++;
+		}
+		else
+		{
+			odd_number_count++;
+		}
+
+		cout << "Enter an integer: ";
+		cin >> input_integer;
+	}
+	cout << "#Even numbers = " << even_number_count << endl;
+	cout << "#Odd numbers = " << odd_number_count << endl;
+	return 0;
 }
